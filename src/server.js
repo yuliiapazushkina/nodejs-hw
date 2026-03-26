7e1fc02cdc51688b5a0d530062836d85a1f008d6
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2026-03-26 19:01:18 +0100

    fix

--- src/server.js
+++ src/server.js
@@ -8,21 +8,23 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
-app.use(pino());
 
-// Routes
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  })
+);
 
-// GET /notes
 app.get('/notes', (req, res) => {
   res.status(200).json({
     message: 'Retrieved all notes',
   });
 });
 
-// GET /notes/:noteId
 app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
 
@@ -31,26 +33,23 @@ app.get('/notes/:noteId', (req, res) => {
   });
 });
 
-// TEST ERROR
 app.get('/test-error', () => {
   throw new Error('Simulated server error');
 });
 
-// 404 middleware
 app.use((req, res) => {
   res.status(404).json({
     message: 'Route not found',
   });
 });
 
-// 500 error handler
 app.use((err, req, res, next) => {
+  console.error(err);
   res.status(500).json({
     message: err.message,
   });
 });
 
-// Server start
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
