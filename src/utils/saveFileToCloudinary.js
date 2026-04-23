1e687ac7478d86de0eec0e5169ad3ab9d29af8ee
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2026-04-23 13:54:59 +0200

    hw5

---
+++ src/utils/saveFileToCloudinary.js
@@ -0,0 +1,29 @@
+import { Readable } from 'node:stream';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  secure: true,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export async function saveFileToCloudinary(buffer) {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: 'avatars',
+        resource_type: 'image',    
+        overwrite: true,
+        unique_filename: true,
+        use_filename: true,
+      },
+      (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      },
+    );
+
+    Readable.from(buffer).pipe(uploadStream);
+  });
+}
