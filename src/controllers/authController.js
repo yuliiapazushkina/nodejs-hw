5c26a27b6afbb61561184958132db20d29e78a89
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2026-04-23 14:23:25 +0200

    hw5

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
-import { User } from '../models/user.js';
+import User from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import jwt from 'jsonwebtoken';
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import createHttpError from "http-errors";
-import { User } from "../models/user.js";
+import User from '../models/user.js';
 import { saveFileToCloudinary } from "../utils/saveFileToCloudinary.js";
 
 export const updateUserAvatar = async (req, res, next) => {
