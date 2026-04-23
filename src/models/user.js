edeb0df58339b2c218c98765bcbe8692453f18ce
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2026-04-23 14:20:01 +0200

    hw5

--- src/models/user.js
+++ src/models/user.js
@@ -2,16 +2,32 @@ import { model, Schema } from 'mongoose';
 
 const userSchema = new Schema(
   {
-    username: { type: String, trim: true },
-    email: { type: String, unique: true, required: true, trim: true },
-    password: { type: String, required: true, minlength: 8 },
+    username: {
+      type: String,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+
     avatar: {
       type: String,
-      required: false,
-      default: "https://ac.goit.global/fullstack/react/default-avatar.jpg",
+      default: 'https://ac.goit.global/fullstack/react/default-avatar.jpg',
     },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+  },
 );
 
 userSchema.methods.toJSON = function () {
@@ -24,7 +40,8 @@ userSchema.pre('save', function (next) {
   if (!this.username) {
     this.username = this.email;
   }
+
   next();
 });
 
-export const User = model('User', userSchema);
+export default model('User', userSchema);
